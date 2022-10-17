ace8428d1f8f55c430e0be2a8737280e9d466b2a
BimbaMan <ivan.d.sidorenko@gmail.com>
2022-10-17 15:48:20 +0300

    some new lessons

--- FirstProgram/JsConsoleBasics.js
+++ FirstProgram/JsConsoleBasics.js
@@ -59,3 +59,48 @@
 // console.log(a.toFixed(2)); //Выведет строку '0.30'
 // console.log(+a.toFixed(2)); //Выведет число 0.3
 
+// //Функция prompt();
+// //позволяет получить от пользователя какой-либо текст
+// let name = prompt('What\'s your name?');
+// alert('Your name: ' + name);
+// alert('Your name: ' + prompt('What\'s your name?'));
+// alert('Your age: ' + prompt('What\'s your age?')); // функция prompt() всегда возвращает строку!
+// let num1 = prompt("Enter first number: ");
+// let num2 = prompt("Enter second number: ");
+// alert(Number(num1) + Number(num2));
+// let a = prompt("Enter first side of the rectangle");
+// let b = prompt("Enter second side of the rectangle");
+// let perimeterRect = a*2 + b*2;
+// alert("Perimeter is: " + perimeterRect);
+
+//Ввод текста в документ
+// document.writeln(123);
+// document.writeln('some text');
+// document.writeln('<b>text in tag</b>');
+// //вывод текста в колонку
+// document.write('text1<br>');
+// document.write('text2<br>');
+// document.write('text3<br>')
+// //второй вариант текста в колонку
+// document.write('text1');
+// document.write('<br>');
+// document.write('text2');
+// //вывод текста курсивом
+// document.write('<i>Text</i>');
+
+// //Как я понял мини квиз "Найди ошибку в коде и исправь её" (хотя там нет ничего интересного для меня)
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (num1 + num2));
+//
+// let num = '123';
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+// console.log(sum);
+
+// //Какая-то скучная практика на операции в JS, делать больше трёх штук просто не вижу смысла
+// console.log(60*60*60*24); //сколько секунд в сутках
+// console.log(60*24*365); //минут в году
+// console.log(1024**4); //байт в терабайте
+
+//Практика на формулы
+//Мне лень делать дальше, поэтому to be continued...
